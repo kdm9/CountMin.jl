815df850777ab7a34ccba6e41d27b8b703a94340
Kevin Murray <spam@kdmurray.id.au>
2016-04-04 16:05:19 +1000

    Remove append/unappend of cms

--- src/CountMin.jl
+++ src/CountMin.jl
@@ -232,19 +232,4 @@ function readcms(filename::AbstractString)
 end
 
 
-function append!(a::CountMinSketch, b::CountMinSketch)
-    if shape(a) != shape(b)
-        error("CountMinSketches must be of the exact same sketch dimensions")
-    end
-    a.sketch += b.sketch
-end
-
-
-function unappend!(a::CountMinSketch, b::CountMinSketch)
-    if shape(a) != shape(b)
-        error("CountMinSketches must be of the exact same sketch dimensions")
-    end
-    a.sketch -= b.sketch
-end
-
 end # module CountMin
