820ee582511c217616b089d7045de8a0a77f9f65
Kevin Murray <spam@kdmurray.id.au>
2016-06-13 18:16:59 +1000

    Update benchmark script

--- misc/cms.jl
+++ misc/cms.jl
@@ -2,11 +2,13 @@ using DataStructures
 using CountMin
 
 
-N = 100000
+N = 100
+N = 10000000
 
 function dotest(cms, n::Integer)
     while n > 0
-        push!(cms, n-=1)
+        push!(cms, n)
+        n -= 1
     end
 end
 
@@ -23,14 +25,15 @@ dotest(ctr, 1000)
 
 print("Checking equality... ")
 for i in 1:N
-    assert(cms[i] == ctr[i])
+    assert(cms[i] >= ctr[i])
 end
 println("All Good!")
 
 function sumtest(cms, n)
-    sm = 0
-    for i in 1:N
-        sm += cms[i]
+    a::UInt8 = 0
+    while n > 0
+        a = cms[n]
+        n -= 1
     end
 end
 
@@ -44,4 +47,16 @@ Profile.print(maxdepth=15)
 println("Profiling getitem")
 Profile.clear()
 @profile sumtest(cms, N)
+Profile.print(maxdepth=15)
+
+function oflow(cms, n::Integer)
+    while n > 0
+        add!(cms, 255)
+        n -= 1
+    end
+end
+
+println("Profiling add with overflow")
+Profile.clear()
+@profile oflow(cms, 10000)
 Profile.print(maxdepth=15)
